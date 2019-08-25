8c25a144c389a6d749575f5b9458771151cd7058
fearghusson <53100380+fearghusson@users.noreply.github.com>
2019-08-25 19:34:09 -0400

    Update random password gen.py

--- random password gen.py
+++ random password gen.py
@@ -1,52 +1,51 @@
-import random
-
-letters = {1: 'a', 2: 'b', 3: 'c', 4:'d', 5:'e', 6:'f', 7:'g',8:'h', 9:'g',
+def randpass(letter = 7, number = 1, case = 1, special = 1):
+    import random
+    
+    letters = {1: 'a', 2: 'b', 3: 'c', 4:'d', 5:'e', 6:'f', 7:'g',8:'h', 9:'g',
            10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q',
            18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y',
            26:'z'}
-numbers = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8',
-           9:'9'}
-password = ""
-password_letter = ""
-password_number = ""
-sorted_password = ""
-blank_list = []
-
-
-print('just letters? yes or no')
-
-yes_no = input()
-
-if yes_no.lower() == 'yes':
+    numbers = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8',
+               9:'9'}
+    specials = {1:'!', 2:'@', 3:'#', 4:'$', 5:'%', 6:'^', 7:'&', 8:'*'}
     
-   print('how long is the password?')
-   all_letter_pass = int(input())
-   
-   for i in range(all_letter_pass):
-       password += str(letters[(random.randint(1, 26))])
-   print(password)
-   
-elif yes_no.lower() == 'no':
-    print('how many letters?')
-    number_of_letters = int(input())
+    #blank lists to hold info
+    pass_letter = ''
+    pass_number = ''
+    sorted_password = ''
+    blank_list = []
+    unsorted_pass = ''
+    pass_special = ''
+    pass_upper = ''
     
-    print('how many numbers?')
-    number_of_numbers = int(input())
-    
-    for iaa in range(number_of_letters):
-        password_letter += str(letters[(random.randint(1, 26))])
+    #loops that fill the blank lists
+    for iaa in range(letter):
+        pass_letter += str(letters[(random.randint(1, 26))])
         
-    for ibb in range(number_of_numbers):
-        password_number += str(numbers[(random.randint(0, 9))])
-        
-    unsorted_password = password_letter + password_number
+    for ibb in range(number):
+        pass_number += str(numbers[(random.randint(0, 9))])
     
-    for icc in range(len(unsorted_password)):
+    for igg in range(special):
+        pass_special += str(specials[(random.randint(1,8))])
+    
+    for ijj in range(case):
+        pass_upper += str(letters[(random.randint(1, 26))])
+    
+    #combinded from each now filled list
+    unsorted_pass = pass_letter + pass_number+ pass_special + pass_upper.upper()
+    
+    #takes len of unsorted_pass and takes the number value for each item in 
+    #the password so that it can be randomized. blank list will look like
+    #[0,1,2,3,4]
+    for icc in range(len(unsorted_pass)):
         blank_list.append(icc)
-        
+    #this wil shuffle blank_list to look like [3,0,1,4,2]
     random.shuffle(blank_list)
     
+    #this loop moves the values from the unsorted_pass based on the shuffled
+    #blank_list
     for idd in blank_list:
-        sorted_password += unsorted_password[blank_list[idd]]
-    
+        sorted_password += unsorted_pass[blank_list[idd]]
+        
     print(sorted_password)
+    return sorted_password
